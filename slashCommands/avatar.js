23593b04a85b114a52a1a60f86a8b334b0b9bfdd
Hasan Ahmed <wajie28@gmail.com>
2025-09-04 14:17:20 +0530

    push

--- slashCommands/avatar.js
+++ slashCommands/avatar.js
@@ -1,13 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("avatar")
     .setDescription("Display a user's avatar")
-    .addUserOption(option => option.setName("user").setDescription("User to show avatar of").setRequired(false)),
+    .addUserOption(option => 
+      option.setName("user")
+        .setDescription("User to show avatar of")
+        .setRequired(false)
+    ),
   async execute(interaction) {
     const user = interaction.options.getUser("user") || interaction.user;
 
-    await interaction.reply({ content: `${user.tag}'s avatar: ${user.displayAvatarURL({ dynamic: true, size: 1024 })}` });
+    const embed = new EmbedBuilder()
+      .setTitle(`🖼 Avatar of ${user.username}`)
+      .setColor(0x00AEEF)
+      .setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+      .setFooter({ text: "PingPal • 2025", iconURL: interaction.client.user.avatarURL() })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
